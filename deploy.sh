7821ed85285d08e806d668778408e514603f6192
İlyas Ziyaoglu <ilyasziyaoglu@gmail.com>
2022-03-31 00:31:28 +0300

    testler silindi, registry kaldırıldı, eureka ve discovery server bağımlılıkları kaldırıldı

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-./mvnw package -Pprod verify -DskipTests jib:dockerBuild --offline
-docker-compose -f src/main/docker/app.yml up
+./mvnw package -Pprod verify -DskipTests jib:dockerBuild
+docker-compose -f src/main/docker/app-prod.yml up
 
 docker network disconnect lawfirm-network docker_lawfirm-app_1
 docker network disconnect lawfirm-network docker_lawfirm-postgresql_1
-docker network disconnect lawfirm-network docker_jhipster-registry_1
 
 docker network rm lawfirm-network
 docker network create --subnet 172.20.0.0/16 lawfirm-network
 
-docker network connect --ip 172.20.0.2 lawfirm-network docker_jhipster-registry_1
 docker network connect --ip 172.20.0.3 lawfirm-network docker_lawfirm-postgresql_1
 docker network connect --ip 172.20.0.4 lawfirm-network docker_lawfirm-app_1
 
