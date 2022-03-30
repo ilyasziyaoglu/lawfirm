d1f84e3fc8206f4e48505c7fda78d5d991c29fa3
İlyas Ziyaoglu <ilyasziyaoglu@gmail.com>
2022-03-30 12:14:12 +0300

    removed container sleep times

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,4 @@ docker network connect --ip 172.20.0.3 lawfirm-network docker_lawfirm-postgresql
 docker network connect --ip 172.20.0.4 lawfirm-network docker_lawfirm-app_1
 
 docker network inspect lawfirm-network
+
